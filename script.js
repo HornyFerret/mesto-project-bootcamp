75ed93f142ff1b4d2270637cded48131543fe234
HornyFerret <HornyFerret@yandex.ru>
2023-10-02 06:31:02 +0300

    Сделала JS, но поломала стили

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+let saveButton = document.querySelector('.popup__button');
+
+function addInput() {
+    let name = document.querySelector('.popup__input-first');
+    let profesion = document.querySelector('.popup__input-second');
+    let nameNew = document.querySelector('.profile__main-text');
+    let profesionNew = document.querySelector('.profile__text');
+  
+    nameNew.textContent = `${name.value}`;
+    profesionNew.textContent = `${profesion.value}`;
+
+    name.value = '';
+    profesion.value = '';
+}
+saveButton.addEventListener('click', addInput);
