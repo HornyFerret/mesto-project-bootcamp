3d5e6ea07bb605bffc12135c649cb7fb838bb208
HornyFerret <HornyFerret@yandex.ru>
2023-10-10 04:58:21 +0300

    im fixed massive?...

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const changeName = document.querySelector('.profile__info-picture');
-const openPopup = document.querySelector('.popup');
-const closeChangeName = document.querySelector('.popup__close');
+const addPlace = document.querySelector('.profile__add-button');
+const popup = document.querySelector('.popup');
+const closePopup = document.querySelector('.popup__close');
+const saveButton = document.querySelector('.popup__button');
+
 const initialCards = [
     {
       name: 'Архыз',
@@ -28,36 +31,86 @@ const initialCards = [
     }
   ];
 
+// function for all page
+function openPlace(item) {
+  item.classList.add("popup_opened");
+};
+function closePlace(item) {
+  item.classList.remove("popup_opened");
+};
+
+  // change name
+let nameChange = document.getElementById("name");
+let profesion = document.getElementById("proffesion");
+let nameNew = document.querySelector('.profile__main-text');
+let profesionNew = document.querySelector('.profile__text');
+
 changeName.addEventListener('click', function () {
-    const saveButton = document.querySelector('.popup__button');
-    let name = document.getElementById("name");
-    let profesion = document.getElementById("proffesion");
-    let nameNew = document.querySelector('.profile__main-text');
-    let profesionNew = document.querySelector('.profile__text');
+  openPlace(popup);
+  nameChange.value = nameNew.textContent;
+  profesion.value = profesionNew.textContent;
+}); 
 
-    openPopup.classList.add("popup_opened");
-
-    name.value = nameNew.textContent;
-    profesion.value = profesionNew.textContent;
-
-
-    function addInput() {
-        nameNew.textContent = `${name.value}`;
-        profesionNew.textContent = `${profesion.value}`;
-
-        name.value = '';
-        profesion.value = '';
-    }
-
-    saveButton.addEventListener('click', function () {
-        addInput();
-        openPopup.classList.remove("popup_opened");
-    });
-  }); 
-
-    
-closeChangeName.addEventListener('click', function () {
-    openPopup.classList.remove("popup_opened");
+saveButton.addEventListener('click', function () {
+  nameNew.textContent = `${nameChange.value}`;
+  profesionNew.textContent = `${profesion.value}`;
+  closePlace(popup);
 });
+
+closePopup.addEventListener('click', function () {
+  closePlace(popup);
+});
+
+// like
+
+const likeButton = document.querySelectorAll('#like');
+let likeButtonMassive = Array.from(likeButton);
+
+likeButtonMassive.forEach(function (item) {
+  item.addEventListener('click', function (evt){
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle('element__like_active');
+  });
+  return item;
+});
+
+// add place card
+
+const namePlace = document.getElementById("name-place");
+const linkPicture = document.getElementById("link-picture");
+const openPlacePopup = document.getElementById("place");
+const closePlacePopup = document.getElementById("place-close");
+const savePlacePopup = document.getElementById("place-save");
+const elements = document.querySelector('.elements');
+const element = document.querySelector('#new_element').content;
+
+
+addPlace.addEventListener('click', function(){
+  openPlace(openPlacePopup);
+});
+
+closePlacePopup.addEventListener('click', function(){
+  closePlace(openPlacePopup);
+});
+
+function changeElement(itemText, itemPhoto) {
+  const elementClone = element.querySelector('.element').cloneNode(true);
+  elementClone.querySelector('.element__text').textContent = itemText;
+  elementClone.querySelector('.element__image').src = itemPhoto;
+  return elementClone;
+};
+
+function addOnElements(element) {
+  elements.prepend(element);
+};
+
+initialCards.forEach((item) => {
+ addOnElements(changeElement(item.name, item.link));
+});
+
+
+
+
+
 
 
