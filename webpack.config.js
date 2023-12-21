aba3ca2aa29d53ed7ea950e1e2a02e7fbac17334
HornyFerret <HornyFerret@yandex.ru>
2023-12-21 14:06:11 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,53 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
+module.exports = {
+  entry: {
+    main: './src/components/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
 
-
-const stylesHandler = MiniCssExtractPlugin.loader;
-
-
-
-const config = {
-    entry: './src/components/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-        }),
-
-        new MiniCssExtractPlugin(),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler, 'css-loader', 'postcss-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
-    resolve: {
-        fallback: {
-        assert: require.resolve('assert'),
-        buffer: require.resolve('buffer'),
-        console: require.resolve('console-browserify'),
-        constants: require.resolve('constants-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        domain: require.resolve('domain-browser'),
-        events: require.resolve('events'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        path: require.resolve('path-browserify'),
-        punycode: require.resolve('punycode'),
-        process: require.resolve('process/browser'),
-        querystring: require.resolve('querystring-es3'),
-        stream: require.resolve('stream-browserify'),
-        string_decoder: require.resolve('string_decoder'),
-        sys: require.resolve('util'),
-        timers: require.resolve('timers-browserify'),
-        tty: require.resolve('tty-browserify'),
-        url: require.resolve('url'),
-        util: require.resolve('util'),
-        vm: require.resolve('vm-browserify'),
-        zlib: require.resolve('browserify-zlib'),
-        "fs": false,
-        "child_process": false,
-        }
-    },
-    stats: {
-        errorDetails: true,
-    }
-};
-
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-    } else {
-        config.mode = 'development';
-    }
-
-    return config;
-};
+  ]
+}
